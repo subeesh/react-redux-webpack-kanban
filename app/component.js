01944daddf35ef6407ab38050b1759b1c155e3c3
subeesh <subeesh.a@gmail.com>
2015-12-16 00:11:02 +0800

    hello world

---
+++ app/component.js
@@ -0,0 +1,6 @@
+module.exports = function() {
+	var el = document.createElement('h1');
+	el.innerHTML = 'Hello World';
+
+	return el;
+}
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+var path = require('path');
+var HtmlwebpackPlugin = require('html-webpack-plugin');
+
+const PATHS = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'build')
+};
+
+module.exports = {
+  // Entry accepts a path or an object of entries.
+  entry: PATHS.app,
+  output: {
+    path: PATHS.build,
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new HtmlwebpackPlugin({
+      title: 'Kanban app'
+    })
+  ]
+};
